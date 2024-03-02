724cb71ef77e77d225ce15f123156a910e621b7a
evan8383 <evanbennett83@gmai.com>
2024-03-02 07:51:58 -0500

    added basic functionality to highlight password fields that dont match on submit

--- Client/src/pages/Signup.jsx
+++ Client/src/pages/Signup.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { useMutation } from '@apollo/client'
@@ -14,72 +14,84 @@ import MailIcon from '../components/MailIcon'
 const Signup = () => {
   const [formState, setFormState] = useState({ username: '', email: '', password: '' })
   const [Cpassword, setCpassword] = useState('')
-  const [addUser, { error, data }] = useMutation(SIGNUP_USER)
 
+  const [matchPassword, setMatchPassword] = useState(null)
+
+  const [addUser, { error, data }] = useMutation(SIGNUP_USER)
 
   const handleConfirmPassword = (event) => {
     const { name, value } = event.target
     setCpassword({ ...Cpassword, [name]: value })
   }
+
   const handleFormChange = (event) => {
     const { name, value } = event.target
     setFormState({ ...formState, [name]: value })
   }
-
 
   const handleFormSubmit = async (event) => {
     event.preventDefault()
     try {
       // * improve this
       if (Cpassword.Cpassword !== formState.password) {
-        alert('Passwords do not match!')
-        return
+        setMatchPassword(false)
       }
+      setMatchPassword(true)
       const { data } = await addUser({ variables: { ...formState } })
       Auth.login(data.addUser.token)
     } catch (e) {
       console.error(e)
     }
   }
+
+  useEffect(() => {
+    setMatchPassword(null)
+  }, [Cpassword || formState])
+
   return (
     <>
-      <div className='bg-black w-fit m-auto p-10 h-lvh'>
+      <div className='bg-black w-full m-auto p-10 h-lvh'>
+        <div className="max-w-fit m-auto h-full">
+          <h4 className='text-white text-center text-2xl font-semibold'>Create Your Account!</h4>
+          <p className='text-white text-sm text-center w-56 m-auto'>You're personal performance tracker is only a few clicks away!</p>
 
-        <h4 className='text-white text-center text-2xl font-semibold'>Create Your Account!</h4>
-        <p className='text-white text-sm text-center w-56 m-auto'>You're personal performance tracker is only a few clicks away!</p>
+          <form className='text-white flex flex-wrap text-center justify-center mb-4' onSubmit={handleFormSubmit}>
+            <div className=" flex flex-wrap justify-center w-full py-4">
+              <div className='flex w-full bg-gray-500 rounded'>
+                <UserIcon className='w-6 h-6 m-auto ml-1' />
+                <input className="bg-gray-500 p-1 rounded border-1 border-white placeholder:text-white w-full outline-none" placeholder='Username' type="text" name="username" id="username" onChange={handleFormChange} />
+              </div>
+            </div>
 
-        <form className='text-white flex flex-wrap text-center justify-center mb-4' onSubmit={handleFormSubmit}>
-          <div className=" flex flex-wrap justify-center w-full py-4">
-            <div className='flex w-full bg-gray-500 rounded'>
-              <UserIcon className='w-6 h-6 m-auto ml-1' />
-              <input className="bg-gray-500 p-1 rounded border-1 border-white placeholder:text-white w-full outline-none" placeholder='Username' type="text" name="username" id="username" onChange={handleFormChange} />
+            <div className=" flex flex-wrap justify-center w-full py-4">
+              <div className='flex w-full bg-gray-500 rounded'>
+                <MailIcon className='w-6 h-6 m-auto ml-1' />
+                <input className="bg-gray-500 p-1 rounded border-1 border-white placeholder:text-white w-full outline-none" placeholder='Email' type="text" name="email" id="email" onChange={handleFormChange} />
+              </div>
             </div>
-          </div>
 
-          <div className=" flex flex-wrap justify-center w-full py-4">
-            <div className='flex w-full bg-gray-500 rounded'>
-              <MailIcon className='w-6 h-6 m-auto ml-1' />
-              <input className="bg-gray-500 p-1 rounded border-1 border-white placeholder:text-white w-full outline-none" placeholder='Email' type="text" name="email" id="email" onChange={handleFormChange} />
+            <div className=" flex flex-wrap justify-center w-full py-4">
+              <div className='flex w-full bg-gray-500 rounded'>
+                <LockIcon className='w-6 h-6 m-auto ml-1' />
+                <input className="bg-gray-500 p-1 rounded border-1 border-white placeholder:text-white w-full outline-none" placeholder='Password' type="text" name="password" id="password" onChange={handleFormChange} />
+              </div>
             </div>
-          </div>
+            <div className=" flex flex-wrap justify-center w-full py-4">
+              <div className='flex w-full bg-gray-500 rounded'>
+                <LockIcon className='w-6 h-6 m-auto ml-1' />
 
-          <div className=" flex flex-wrap justify-center w-full py-4">
-            <div className='flex w-full bg-gray-500 rounded'>
-              <LockIcon className='w-6 h-6 m-auto ml-1' />
-              <input className="bg-gray-500 p-1 rounded border-1 border-white placeholder:text-white w-full outline-none" placeholder='Password' type="text" name="password" id="password" onChange={handleFormChange} />
+                <input className={!matchPassword ? "bg-gray-500 p-1 rounded border-1 border-white placeholder:text-white w-full outline-none" :
+                  "bg-gray-500 p-1 rounded border-1 placeholder:text-white w-full outline-none outline-red-500"}
+
+                  placeholder='Confirm Password' type="text" name="Cpassword" id="Cpassword" onChange={handleConfirmPassword} />
+              </div>
             </div>
+            <button type="submit" className="w-fit py-1 px-4 rounded bg-orange-500 align-center font-bold">Sign Up!</button>
+          </form>
+          <div className='flex flex-wrap gap-1 justify-center'>
+            <h5 className="w-full text-white text-center">Already have an account?</h5>
+            <Link to="/login" className='w-fit text-blue-600 hover:underline font-bold'>Log in!</Link>
           </div>
-          <div className=" flex flex-wrap justify-center w-full py-4">
-            <div className='flex w-full bg-gray-500 rounded'>
-              <LockIcon className='w-6 h-6 m-auto ml-1' />
-              <input className="bg-gray-500 p-1 rounded border-1 border-white placeholder:text-white w-full outline-none" placeholder='Confirm Password' type="text" name="Cpassword" id="Cpassword" onChange={handleConfirmPassword}/>
-            </div>
-          </div>
-          <button type="submit" className="w-fit py-1 px-4 rounded bg-orange-500 align-center font-bold">Sign Up!</button>
-        </form>
-        <div className='flex flex-wrap gap-1 justify-center'>
-          <h5 className="w-full text-white text-center">Already have an account?</h5>
-          <Link to="/login" className='w-fit text-blue-600 hover:underline font-bold'>Log in!</Link>
         </div>
       </div>
     </>
