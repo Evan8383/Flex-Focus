2d678a86f2184f03558d012446b8c8566f71529f
evan8383 <evanbennett83@gmai.com>
2024-03-05 21:33:46 -0500

    added routing for single exercise page.

--- Client/src/pages/MyWorkouts.jsx
+++ Client/src/pages/MyWorkouts.jsx
@@ -2,12 +2,11 @@ import Auth from '../utils/auth'
 import { useQuery } from '@apollo/client'
 import { useState, useEffect } from 'react'
 import { useMutation } from '@apollo/client'
-import { Link } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 
 import { GET_WORKOUTS } from '../utils/queries'
 
 export default function MyWorkouts() {
-
   const userId = Auth.getUserAccount().data._id
   const { loading, data, error } = useQuery(GET_WORKOUTS, {
     variables: { userId: userId },
@@ -15,8 +14,7 @@ export default function MyWorkouts() {
   })
   const workoutData = data?.getOneUserAccount || []
 
-  console.log(workoutData)
-
+  let { workoutId } = useParams()
 
   return (
     <>
@@ -35,24 +33,26 @@ export default function MyWorkouts() {
 
         {workoutData.workouts.length ? workoutData.workouts.map((workout) => (
           <div key={workout._id} className="z-50 mx-auto grid max-w-2xl gap-4 px-4 cursor-pointer min-w-full mb-4 z-10">
-            <div className="rounded-xl border overflow-hidden divide-y bg-zinc-600">
-              <div className="flex items-center p-4">
-                <h2 className="font-bold">{workout.workoutName}</h2>
-                <p className="ml-auto text-sm white">{workout.workoutCategory ? workout.workoutCategory : 'Uncategorized'}</p>
+            <Link to={`/app/workouts/${workout._id}`}>
+              <div className="rounded-xl border overflow-hidden divide-y bg-zinc-600">
+                <div className="flex items-center p-4">
+                  <h2 className="font-bold">{workout.workoutName}</h2>
+                  <p className="ml-auto text-sm white">{workout.workoutCategory ? workout.workoutCategory : 'Uncategorized'}</p>
+                </div>
+                <div className="p-4">
+                  <p className="text-sm text-white ">
+                    {workout.workoutDescription ? workout.workoutDescription : "No description available"}
+                  </p>
+                </div>
               </div>
-              <div className="p-4">
-                <p className="text-sm text-white ">
-                  {workout.workoutDescription ? workout.workoutDescription : "No description available"}
-                </p>
-              </div>
-            </div>
+            </Link>
             <div className='flex justify-around'>
               <button className='py-2 px-5 bg-zinc-600 rounded'>Details</button>
               <button className='py-2 px-5 bg-orange-500 rounded'>Delete</button>
             </div>
           </div>
         )) : <div className="mx-auto flex min-h-[400px] items-center justify-center p-4">No workouts to display</div>}
-      </div>
+      </div >
     </>
   )
 }
