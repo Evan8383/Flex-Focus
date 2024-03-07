bc7b5d5e26ff068a05ddcba575afd09453846218
evan8383 <evanbennett83@gmai.com>
2024-03-06 19:30:47 -0500

    handled auto signout and redirect on token expire

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 const secret = process.env.JWT_SECRET || process.env.SECRET || 'mysecret';
-const expiration = '10s';
+const expiration = '24h';
 
 const AuthenticationError = new GraphQLError('Could not authenticate user', {
   extensions: {
